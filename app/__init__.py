0edcc4708a7777a1ece0857febbcd0516fe5cf2a
PramodSaraswat <37214138+PramodSaraswat@users.noreply.github.com>
2020-09-15 21:50:58 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -60,7 +60,7 @@ def create_app(config_class=Config):
 			mail_handler=SMTPHandler(
 				mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
 				fromaddr='no-reply@' + app.config['MAIL_SERVER'],
-				toaddrs=app.config['ADMINS'], subject='Microblog Failure',
+				toaddrs=app.config['ADMINS'], subject='App Failure',
 				credentials=auth, secure=secure)
 			mail_handler.setLevel(logging.ERROR)
 			app.logger.addHandler(mail_handler)
