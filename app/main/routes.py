f5c8edb106a55bc1fbf113a2d3d674acca74604b
PramodSaraswat <8441918081pramod@gmail.com>
2020-09-16 18:25:29 +0530

    on hover profile display function added

--- app/main/routes.py
+++ app/main/routes.py
@@ -144,3 +144,11 @@ def search():
 	prev_url = url_for('main.search', q=g.search_form.q.data, page=page - 1) \
 		if page > 1 else None
 	return render_template('search.html', title=_('Search'), posts=posts,next_url=next_url, prev_url=prev_url)
+
+
+@bp.route('/user/<username>/popup')
+@login_required
+def user_required(username):
+	user=User.query.filter_by(username=username).first_or_404()
+	form=EmptyForm()
+	return render_template('user_popup.html',user=user,form=form)
